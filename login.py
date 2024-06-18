6a7a1b1c36c84531c8999d46ed95ac08831a3d56
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 11:35:03 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,21 +1,44 @@
 import streamlit as st
 from PIL import Image
-import base64
 
 def login(encoded_logo, User, Password):
+    # Cargar el logo de la universidad
     university_logo = Image.open("logo_upch.png")
 
+    # Establecer el fondo de pantalla
     set_background()
 
+    # Centrar el contenido principal
+    st.markdown(
+        """
+        <style>
+        .centered {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Contenedor principal centrado
+    st.markdown('<div class="centered">', unsafe_allow_html=True)
+
+    # Mostrar el logo, título y subheader
     st.image(university_logo, width=200)
     st.title("🎓 Plataforma de Gestión de Cursos - UPCH")
     st.subheader("Inicio de Sesión")
 
-    with st.form(key="login_form"):
+    # Formulario de inicio de sesión
+    with st.form(key="login_form", class_="login-form"):
         username = st.text_input("Usuario:", value="")
         password = st.text_input("Contraseña:", type="password", value="")
         submit = st.form_submit_button("Iniciar Sesión")
 
+    # Procesamiento del formulario
     if submit:
         if username == User and password == Password:
             st.session_state.logged_in = True
@@ -24,6 +47,9 @@ def login(encoded_logo, User, Password):
             st.experimental_rerun()
         else:
             st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
+
+    # Cerrar el contenedor principal centrado
+    st.markdown('</div>', unsafe_allow_html=True)
 
 def set_background():
     background_url = "https://360.cayetano.edu.pe/wp-content/uploads/sites/25/2024/03/53135168333_7b780465e9_k.jpg"
@@ -34,8 +60,8 @@ def set_background():
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
+            height: 100vh;
         }}
         </style>
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
-
