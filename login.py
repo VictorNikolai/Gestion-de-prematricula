f54284384ae80731fb5aadd4c1d23c088e14c7c5
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 11:49:55 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,19 +1,27 @@
+# login.py
 import streamlit as st
 from PIL import Image
+import base64
 
 def login(encoded_logo, User, Password):
+    # Cargar la imagen de la insignia de la universidad
     university_logo = Image.open("logo_upch.png")
 
-    # Centrar título y logo
+    # Establecer el fondo de pantalla
+    set_background()
+
+    # Mostrar logo y título de la aplicación centrados
     st.image(university_logo, width=200, caption='', use_column_width=True, align='center')
     st.title("🎓 Plataforma de Gestión de Cursos - UPCH", align='center')
     st.subheader("Inicio de Sesión", align='center')
 
+    # Formulario de inicio de sesión
     with st.form(key="login_form"):
         username = st.text_input("Usuario:", value="")
         password = st.text_input("Contraseña:", type="password", value="")
         submit = st.form_submit_button("Iniciar Sesión")
 
+    # Procesamiento del formulario
     if submit:
         if username == User and password == Password:
             st.session_state.logged_in = True
@@ -23,3 +31,21 @@ def login(encoded_logo, User, Password):
         else:
             st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
 
+# Función para establecer el fondo de pantalla globalmente
+def set_background():
+    background_url = "https://360.cayetano.edu.pe/wp-content/uploads/sites/25/2024/03/53135168333_7b780465e9_k.jpg"
+    page_bg_img = f"""
+        <style>
+        .stApp {{
+            background-image: url("{background_url}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Ajusta según tus necesidades */
+        }}
+        </style>
+    """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
