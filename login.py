0b22f1bc7b201f52efe373e971a62637497ba5af
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 13:19:43 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,10 @@
-# login.py
 import streamlit as st
 from PIL import Image
 import base64
 
 def login(encoded_logo, User, Password):
-    # Cargar la imagen de la insignia de la universidad
     university_logo = Image.open("logo_upch.png")
-
-    # Establecer el fondo de pantalla
     set_background()
-
-    # Mostrar logo y título de la aplicación centrados
     st.markdown(
         """
         <style>
@@ -39,24 +33,20 @@ def login(encoded_logo, User, Password):
     st.markdown("<div class='title-container'><h1>🎓 Plataforma de Gestión de Cursos - UPCH</h1></div>", unsafe_allow_html=True)
     st.markdown("<div class='subheader-container'><h3>Inicio de Sesión</h3></div>", unsafe_allow_html=True)
 
-    # Formulario de inicio de sesión
     with st.form(key="login_form"):
         username = st.text_input("Usuario:", value="")
         password = st.text_input("Contraseña:", type="password", value="")
         submit = st.form_submit_button("Iniciar Sesión")
 
-    # Procesamiento del formulario
     if submit:
         if username == User and password == Password:
             st.session_state.logged_in = True
             st.success("¡Inicio de sesión exitoso!")
             st.balloons()
-            # Redirigir a la página home después del inicio de sesión
             st.query_params["page"] = "home"
         else:
             st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
 
-# Función para establecer el fondo de pantalla globalmente
 def set_background():
     background_url = "https://raw.githubusercontent.com/VictorNikolai/Gestion-de-prematricula/06d35da9e3a46b3e4c80297a13c319f0097ce5ac/cayetano.png"
     page_bg_img = f"""
