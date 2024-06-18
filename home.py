56ebea06d250af592f305aa7af11cabd7491122d
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 12:44:05 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,13 @@
+# home.py
 import streamlit as st
 
 def app():
-    st.markdown("# Página de Inicio (Home)")
-    st.write("Este es el contenido de la página de inicio.")
-    # Puedes agregar más contenido según sea necesario para la página de inicio
+    st.title("Bienvenido a la Página de Inicio")
+    st.write("Aquí puedes comenzar a explorar las funcionalidades de la plataforma.")
+
+    # Agrega aquí el contenido específico de la página de inicio
+    # Por ejemplo, puedes mostrar opciones o funcionalidades disponibles
+    
+    st.write("¡Explora y disfruta de la plataforma!")
+
+    # Aquí puedes agregar más contenido o funcionalidades según sea necesario
