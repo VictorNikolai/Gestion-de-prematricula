6e9409248c24aeeec13eb6280df6e3c56b3ec5ae
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 10:51:47 -0500

    Update login.py

--- login.py
+++ login.py
@@ -2,33 +2,36 @@ import streamlit as st
 from PIL import Image
 import base64
 
-def get_base64(png_file):
-    with open(png_file, "rb") as f:
-        return base64.b64encode(f.read()).decode()
-
 def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
+    with open(png_file, "rb") as f:
+        bin_str = base64.b64encode(f.read()).decode()
+    page_bg_img = f'''
     <style>
-    .stApp {
-        background-image: url("data:image/png;base64,%s");
+    .stApp {{
+        background-image: url("data:image/png;base64,{bin_str}");
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
-    }
+    }}
     </style>
-    ''' % bin_str
+    '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 def app():
-    # Llamar a set_background con la imagen deseada
-    set_background("universidad.png")  # Asegúrate de que "universidad.png" esté en el mismo directorio
+    # Configurar el fondo de pantalla con la imagen universidad.jpg
+    set_background("universidad.jpg")
 
-    st.title("🎓 Plataforma de Gestión de Cursos - UPCH")
-    st.subheader("Inicio de Sesión")
+    # Cargar la imagen del logo de la universidad (ejemplo)
+    university_logo = Image.open("Logo_upch.png")
 
+    # Credenciales de inicio de sesión (ejemplo)
     User = "41650931"
     Password = "cayetano"
+
+    # Diseño del formulario de inicio de sesión
+    st.image(university_logo, width=200)
+    st.title("🎓 Plataforma de Gestión de Cursos - UPCH")
+    st.subheader("Inicio de Sesión")
 
     with st.form(key="login_form"):
         username = st.text_input("Usuario:", value="")
@@ -40,11 +43,14 @@ def app():
             st.success("¡Inicio de sesión exitoso!")
             st.balloons()
             st.write("""
-                ## Bienvenido a la Plataforma de Gestión de Cursos - UPCH
-                En esta aplicación, podrás explorar los cursos de los 10 ciclos de la carrera de Ingeniería Informática en la Universidad Peruana Cayetano Heredia (UPCH). Descubre los cursos, sus prerrequisitos y detalles para planificar tu trayectoria académica de manera efectiva.
+            ## Bienvenido a la Plataforma de Gestión de Cursos de Ingeniería Informática - UPCH
+            En esta aplicación, podrás explorar los cursos de los 10 ciclos de la carrera de Ingeniería Informática en la Universidad Peruana Cayetano Heredia (UPCH). Descubre los cursos, sus prerrequisitos y detalles para planificar tu trayectoria académica de manera efectiva.
             """)
         else:
             st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
+    else:
+        st.warning("Por favor, inicie sesión para continuar.")
 
 if __name__ == "__main__":
     app()
+
