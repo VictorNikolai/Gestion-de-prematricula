babd88b92f04cf10d3a2ce5264a265832243c90d
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 12:27:48 -0500

    Update home.py

--- home.py
+++ home.py
@@ -2,26 +2,27 @@
 import streamlit as st
 from asignacion_alumnos import app as asignacion_alumnos_app
 from modelar_salones import app as modelar_salones_app
-# Import other apps here as needed
+# Importar otras aplicaciones según sea necesario
 
 def app():
     st.title("Página Principal - Plataforma de Gestión de Cursos")
     
-    # Retrieve query parameters
-    query_params = st.query_params()
-    page = query_params.get("page", "home")  # Default to "home" page if no page specified
+    # Leer los parámetros de consulta
+    query_params = st.experimental_get_query_params()
+    page = query_params.get("page", ["home"])[0]
 
-    # Barra de navegación para seleccionar la sección
-    selected_app = st.selectbox("Selecciona una sección", 
-                                ["Asignación de Alumnos", "Modelar Salones"])  # Add more options as needed
+    if page == "home":
+        # Barra de navegación para seleccionar la sección
+        selected_app = st.selectbox("Selecciona una sección", 
+                                    ["Asignación de Alumnos", "Modelar Salones"])  # Agrega más opciones según sea necesario
+        
+        # Renderizar la aplicación seleccionada
+        if selected_app == "Asignación de Alumnos":
+            asignacion_alumnos_app()
+        elif selected_app == "Modelar Salones":
+            modelar_salones_app()
+        # Añadir más condiciones para otras secciones
 
-    # Execute the selected application
-    if selected_app == "Asignación de Alumnos":
-        asignacion_alumnos_app()
-    elif selected_app == "Modelar Salones":
-        modelar_salones_app()
-    # Add more conditions for other sections
-
-# Main app execution
+# Ejecutar la aplicación principal
 if __name__ == "__main__":
     app()
