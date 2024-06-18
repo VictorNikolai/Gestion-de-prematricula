629c383ce24865acf96db2d351b2795090b9dcb5
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 11:52:55 -0500

    Update login.py

--- login.py
+++ login.py
@@ -10,10 +10,34 @@ def login(encoded_logo, User, Password):
     # Establecer el fondo de pantalla
     set_background()
 
-    # Mostrar logo y título de la aplicación
-    st.image(university_logo, width=200)
-    st.title("🎓 Plataforma de Gestión de Cursos - UPCH")
-    st.subheader("Inicio de Sesión")
+    # Mostrar logo y título de la aplicación centrados
+    st.markdown(
+        """
+        <style>
+        .logo-container {
+            display: flex;
+            justify-content: center;
+        }
+        .title-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+        .subheader-container {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+        </style>
+        """
+        , unsafe_allow_html=True)
+
+    st.markdown("<div class='logo-container'>"
+                "<img src='data:image/png;base64,{}' class='img-fluid' width='200'>"
+                "</div>".format(encoded_logo), unsafe_allow_html=True)
+
+    st.markdown("<div class='title-container'><h1>🎓 Plataforma de Gestión de Cursos - UPCH</h1></div>", unsafe_allow_html=True)
+    st.markdown("<div class='subheader-container'><h3>Inicio de Sesión</h3></div>", unsafe_allow_html=True)
 
     # Formulario de inicio de sesión
     with st.form(key="login_form"):
@@ -46,10 +70,6 @@ def set_background():
             align-items: center;
             height: 100vh; /* Ajusta según tus necesidades */
         }}
-        .centered {{
-            text-align: center;
-        }}
         </style>
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
-
