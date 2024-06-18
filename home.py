c75b2c2437fdab6b6449864f2a9428ff7d919e9f
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 12:53:10 -0500

    Update home.py

--- home.py
+++ home.py
@@ -5,9 +5,20 @@ def app():
     st.title("Bienvenido a la Página de Inicio")
     st.write("Aquí puedes comenzar a explorar las funcionalidades de la plataforma.")
 
-    # Agrega aquí el contenido específico de la página de inicio
-    # Por ejemplo, puedes mostrar opciones o funcionalidades disponibles
-    
-    st.write("¡Explora y disfruta de la plataforma!")
+    st.header("Selecciona una sección para comenzar:")
 
-    # Aquí puedes agregar más contenido o funcionalidades según sea necesario
+    # Botones para cada funcionalidad
+    if st.button("Modelar Salones"):
+        st.experimental_set_query_params(page="modelar_salones")
+    if st.button("Modelar Ambientes"):
+        st.experimental_set_query_params(page="modelar_ambientes")
+    if st.button("Modelar Cursos"):
+        st.experimental_set_query_params(page="modelar_cursos")
+    if st.button("Requerimiento de Ambientes"):
+        st.experimental_set_query_params(page="requerimiento_ambientes")
+    if st.button("Asignación de Alumnos"):
+        st.experimental_set_query_params(page="asignacion_alumnos")
+    if st.button("Optimización de Horarios"):
+        st.experimental_set_query_params(page="optimizar_horarios")
+
+    st.write("¡Explora y disfruta de la plataforma!")
