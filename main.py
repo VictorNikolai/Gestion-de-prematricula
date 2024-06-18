0743de363b7709c89f9e4fa26dcf6d16b4a1b36b
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-06-18 13:40:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,40 +11,43 @@ from optimizar_horarios import app as optimizar_horarios_app
 import os
 import base64
 
-st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_title="Gestión de Cursos UPCH")
+def main():
+    st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_title="Gestión de Cursos UPCH")
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    logo_path = os.path.join(current_dir, "logo_upch.png")
 
-logo_path = os.path.join(current_dir, "logo_upch.png")
+    with open(logo_path, "rb") as image_file:
+        encoded_logo = base64.b64encode(image_file.read()).decode()
 
-with open(logo_path, "rb") as image_file:
-    encoded_logo = base64.b64encode(image_file.read()).decode()
-    
-User = "0000"  # lo cambie porque me da weba escribir toda esa cosa xd
-Password = "0000"  
+    User = "0000"  # Tu usuario
+    Password = "0000"  # Tu contraseña
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
 
-set_background()
+    set_background()
 
-if not st.session_state.logged_in:
-    login(encoded_logo, User, Password)
-else:
-    app = MultiApp()
+    if not st.session_state.logged_in:
+        login(encoded_logo, User, Password)
+    else:
+        app = MultiApp()
 
-    app.add_app("Home", home_app)
-    app.add_app("Modelar Salones", modelar_salones_app)
-    app.add_app("Modelar Ambientes", modelar_ambientes_app)
-    app.add_app("Modelar Cursos", modelar_cursos_app)
-    app.add_app("Requerimiento de Ambientes", requerimiento_ambientes_app)
-    app.add_app("Asignación de Alumnos", asignacion_alumnos_app)
-    app.add_app("Optimización de Horarios", optimizar_horarios_app)
+        app.add_app("Home", home_app)
+        app.add_app("Modelar Salones", modelar_salones_app)
+        app.add_app("Modelar Ambientes", modelar_ambientes_app)
+        app.add_app("Modelar Cursos", modelar_cursos_app)
+        app.add_app("Requerimiento de Ambientes", requerimiento_ambientes_app)
+        app.add_app("Asignación de Alumnos", asignacion_alumnos_app)
+        app.add_app("Optimización de Horarios", optimizar_horarios_app)
 
-    selected_app = st.selectbox("Selecciona una sección", [app['title'] for app in app.apps])
+        selected_app = st.sidebar.selectbox("Selecciona una sección", [app['title'] for app in app.apps])
 
-    for app_page in app.apps:
-        if app_page['title'] == selected_app:
-            app_page['function']()
-            break
+        for app_page in app.apps:
+            if app_page['title'] == selected_app:
+                app_page['function']()
+                break
+
+if __name__ == "__main__":
+    main()
 
