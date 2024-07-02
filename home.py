83b20f46d79c9715e2b1b1187d92a99fc0881f3b
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-07-02 10:21:58 -0500

    Update home.py

--- home.py
+++ home.py
@@ -9,14 +9,8 @@ def app():
             justify-content: center;
             align-items: center;
             text-align: center;
-            height: 100vh;
-            margin-top: -50px;
-        }
-        .horizontal-buttons {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 20px;
+            height: 100vh;  /* Ajusta la altura al 100% del viewport */
+            margin-top: -50px;  /* Ajusta el margen superior para centrar mejor */
         }
         </style>
         """,
@@ -29,24 +23,23 @@ def app():
 
     st.header("Selecciona una sección para comenzar:")
 
-    # Contenedor para los botones horizontales
-    with st.markdown("<div class='horizontal-buttons'>", unsafe_allow_html=True):
-        if st.button("Sobre Cayetano"):
-            st.query_params["page"] = "sobre_cayetano"
-            st.write("Información sobre la historia y misión de la Universidad.")
-        if st.button("Admisión"):
-            st.query_params["page"] = "admision"
-            st.write("Requisitos y procedimientos para el proceso de admisión.")
-        if st.button("Teléfonos de Docentes"):
-            st.query_params["page"] = "telefonos_docentes"
-            st.write("Lista de números de teléfono para contactar a los docentes.")
-        if st.button("Teléfonos de Ayuda"):
-            st.query_params["page"] = "telefonos_ayuda"
-            st.write("Números de contacto para recibir ayuda y soporte técnico.")
-        if st.button("Teléfonos de Administración"):
-            st.query_params["page"] = "telefonos_administracion"
-            st.write("Contactos principales del departamento de administración.")
+    if st.button("Sobre Cayetano"):
+        st.query_params["page"] = "sobre_cayetano"
+        st.write("Información sobre la historia y misión de la Universidad.")
+    if st.button("Admisión"):
+        st.query_params["page"] = "admision"
+        st.write("Requisitos y procedimientos para el proceso de admisión.")
+    if st.button("Teléfonos de Docentes"):
+        st.query_params["page"] = "telefonos_docentes"
+        st.write("Lista de números de teléfono para contactar a los docentes.")
+    if st.button("Teléfonos de Ayuda"):
+        st.query_params["page"] = "telefonos_ayuda"
+        st.write("Números de contacto para recibir ayuda y soporte técnico.")
+    if st.button("Teléfonos de Administración"):
+        st.query_params["page"] = "telefonos_administracion"
+        st.write("Contactos principales del departamento de administración.")
 
     st.write("¡Explora y disfruta de la plataforma!")
 
 app()
+
