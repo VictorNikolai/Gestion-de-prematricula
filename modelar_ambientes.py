3ae41c0b5b21546497ca0309ef62af7b1e0780db
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-07-02 11:35:01 -0500

    Update modelar_ambientes.py

--- modelar_ambientes.py
+++ modelar_ambientes.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
+# Función para cargar los datos desde un archivo CSV si existe, o crear datos iniciales si no existe el archivo
 def load_data():
     if os.path.exists('ambientes.csv'):
         return pd.read_csv('ambientes.csv')
@@ -12,17 +13,22 @@ def load_data():
         }
         return pd.DataFrame(ambientes_data)
 
+# Función para guardar los datos en un archivo CSV
 def save_data(ambientes):
     ambientes.to_csv('ambientes.csv', index=False)
 
+# Función principal de la aplicación
 def app():
     st.title("Modelar Tipos de Ambientes - UPCH")
 
+    # Cargar los datos de los ambientes
     ambientes = load_data()
 
+    # Mostrar los tipos de ambientes disponibles
     st.write("## Tipos de Ambientes Disponibles")
     st.write(ambientes)
     
+    # Formulario para añadir un nuevo tipo de ambiente
     with st.form(key='add_ambiente'):
         st.write("### Añadir Nuevo Tipo de Ambiente")
         tipo = st.text_input("Tipo de Ambiente")
@@ -33,9 +39,10 @@ def app():
             new_ambiente = pd.DataFrame({'Tipo de Ambiente': [tipo], 'Descripción': [descripcion]})
             ambientes = pd.concat([ambientes, new_ambiente], ignore_index=True)
             save_data(ambientes)
-            st.success(f"Tipo de Ambiente {tipo} añadido exitosamente")
+            st.success(f"Tipo de Ambiente '{tipo}' añadido exitosamente")
             st.experimental_rerun()
-#deleteooo xd 
+
+    # Formulario para eliminar un tipo de ambiente
     with st.form(key='delete_ambiente'):
         st.write("### Eliminar Tipo de Ambiente")
         ambiente_a_eliminar = st.selectbox("Selecciona el Tipo de Ambiente a eliminar", ambientes['Tipo de Ambiente'])
@@ -44,11 +51,13 @@ def app():
         if submit_delete:
             ambientes = ambientes[ambientes['Tipo de Ambiente'] != ambiente_a_eliminar]
             save_data(ambientes)
-            st.success(f"Tipo de Ambiente {ambiente_a_eliminar} eliminado exitosamente")
+            st.success(f"Tipo de Ambiente '{ambiente_a_eliminar}' eliminado exitosamente")
             st.experimental_rerun()
 
+    # Mostrar los tipos de ambientes actualizados
     st.write("## Tipos de Ambientes Actualizados")
     st.write(ambientes)
 
 if __name__ == '__main__':
     app()
+
