4a9e71e58e52c865705dc40cf1eb71dcb1d45d6e
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-07-02 10:44:19 -0500

    Update home.py

--- home.py
+++ home.py
@@ -18,8 +18,8 @@ def app():
     )
 
     st.markdown("<h1 class='centered-title'>Bienvenido a la Página de Inicio</h1>", unsafe_allow_html=True)
-
     st.write("Aquí puedes comenzar a explorar las funcionalidades de la plataforma.")
+
 
     st.header("Selecciona una sección para comenzar:")
 
