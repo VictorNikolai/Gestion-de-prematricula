c9ffc26f77995f8e5c105c0c350466b9a7ee77c0
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-07-02 10:47:58 -0500

    Update home.py

--- home.py
+++ home.py
@@ -18,10 +18,8 @@ def app():
     )
 
     st.markdown("<h1 class='centered-title'>Bienvenido a la Página de Inicio</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 class='Para comenzar con la configuracion del ciclo, porfavor dirigase a la parte izquierda superior</h3>", unsafe_allow_html=True)
-
-
-
+    st.markdown("<h3 class='Para comenzar con la configuracion del ciclo, porfavor dirigase a la parte izquierda superior'></h3>", unsafe_allow_html=True)
+    
     st.header("Selecciona una sección para comenzar:")
 
     if st.button("Sobre Cayetano"):
