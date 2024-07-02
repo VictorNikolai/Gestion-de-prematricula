18047cb19e4de30b3a599936a737e661d64d4466
Bryan Alexander Penaloza Huaman <117461904+Alexandeeer1@users.noreply.github.com>
2024-07-02 10:31:28 -0500

    Update home.py

--- home.py
+++ home.py
@@ -12,16 +12,6 @@ def app():
             height: 100vh;  /* Ajusta la altura al 100% del viewport */
             margin-top: -50px;  /* Ajusta el margen superior para centrar mejor */
         }
-
-        .centered-text {
-            text-align: center;
-        }
-
-        .centered-button {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 10px;  /* Espacio entre botones */
-        }
         </style>
         """,
         unsafe_allow_html=True
@@ -29,21 +19,26 @@ def app():
 
     st.markdown("<h1 class='centered-title'>Bienvenido a la Página de Inicio</h1>", unsafe_allow_html=True)
 
-    st.markdown("<div class='centered-text'>Aquí puedes comenzar a explorar las funcionalidades de la plataforma.</div>", unsafe_allow_html=True)
+    st.write("Aquí puedes comenzar a explorar las funcionalidades de la plataforma.")
 
     st.header("Selecciona una sección para comenzar:")
 
-    if st.button("Sobre Cayetano", key="sobre_cayetano"):
+    if st.button("Sobre Cayetano"):
+        st.query_params["page"] = "sobre_cayetano"
         st.write("Información sobre la historia y misión de la Universidad.")
-    if st.button("Admisión", key="admision"):
+    if st.button("Admisión"):
+        st.query_params["page"] = "admision"
         st.write("Requisitos y procedimientos para el proceso de admisión.")
-    if st.button("Teléfonos de Docentes", key="telefonos_docentes"):
+    if st.button("Teléfonos de Docentes"):
+        st.query_params["page"] = "telefonos_docentes"
         st.write("Lista de números de teléfono para contactar a los docentes.")
-    if st.button("Teléfonos de Ayuda", key="telefonos_ayuda"):
+    if st.button("Teléfonos de Ayuda"):
+        st.query_params["page"] = "telefonos_ayuda"
         st.write("Números de contacto para recibir ayuda y soporte técnico.")
-    if st.button("Teléfonos de Administración", key="telefonos_administracion"):
+    if st.button("Teléfonos de Administración"):
+        st.query_params["page"] = "telefonos_administracion"
         st.write("Contactos principales del departamento de administración.")
 
-    st.markdown("<div class='centered-text'>¡Explora y disfruta de la plataforma!</div>", unsafe_allow_html=True)
+    st.write("¡Explora y disfruta de la plataforma!")
 
-app()
+app() 
